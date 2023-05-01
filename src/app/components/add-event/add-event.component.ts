d4ad52bf982bd492ea18c49fccca83a8cd62a42c
ihebbn <mohamediheb.benslama@esprit.tn>
2023-05-01 11:35:31 +0100

    .

--- src/app/components/add-event/add-event.component.ts
+++ src/app/components/add-event/add-event.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from './event.service';
+
 
 @Component({
   selector: 'app-add-event',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEventComponent implements OnInit {
 
-  constructor() { }
+  constructor(private eventService: EventService) { }
+    ngOnInit(): void {
+        throw new Error('Method not implemented.');
+    }
 
-  ngOnInit(): void {
+  addEvent(name: string, type: string, description: string, place: string, date: Date, nbr_of_places: number): void {
+    const newEvent = { nameEvent: name, Type: type, description: description, place: place, date: date, nbr_of_places: nbr_of_places };
+    this.eventService.addEvent(newEvent)
+      .subscribe(event => {
+        console.log(`Event with ID ${event.idEvent} has been added`);
+        // Do something with the new event
+      });
   }
-
 }
---
+++ src/app/components/event.model.ts
@@ -0,0 +1,9 @@
+export class Event {
+  idEvent: number;
+  nameEvent: string;
+  Type: string;
+  description: string;
+  place: string;
+  date: Date;
+  nbr_of_places: number;
+}
